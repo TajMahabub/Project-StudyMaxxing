2ef2ce5b66eb8017464e0cc694fe356f5c20bbc7
Taj Mahabub <studybuddy@studymaxxing.app>
2025-11-20 12:34:47 -0330

    Project_StudyMaxxing

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-black">
+        StudyMaxxing – AI Study Buddy
+      </h1>
+    </main>
+  );
+}
